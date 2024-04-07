eeffdcf8c817cc2e3b2faf42004943f078218d2e
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2024-04-07 18:20:40 +0200

    Add abstract class for problem resolver

---
+++ ProblemResolver/ProblemResolver.cs
@@ -0,0 +1,5 @@
+public abstract class ProblemResolver
+{
+    public abstract ProblemResolverResult Resolve(ProblemResolverData data);
+
+}
---
+++ ProblemResolver/ProblemResolverResult.cs
@@ -0,0 +1,4 @@
+abstract public class ProblemResolverResult
+{
+
+}
