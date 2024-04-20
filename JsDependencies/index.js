e5e0a9f297fd79df642b7986d21b3117e2717c1e
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-04-21 01:11:54 +0200

    Resolve icons using npm

--- JsDependencies/index.js
+++ JsDependencies/index.js
@@ -1,1 +1,2 @@
 import "animate.css"
+import "@fortawesome/fontawesome-free/css/all.css"
--- JsDependencies/webpack.config.js
+++ JsDependencies/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, '../wwwroot/'),
+        path: path.resolve(__dirname, '../wwwroot/dependencies'),
         filename: 'JsDependencies.js', 
     },
     module: {
