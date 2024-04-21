127b88a790baff162cfd986e205a68d5e6ce091f
6ajmon <6ajmon@gmail.com>
2024-04-21 22:35:54 +0200

    Add CarrierAssignmentDrawer.cs

---
+++ ProblemDrawer/CarrierAssignmentDrawer.cs
@@ -0,0 +1,14 @@
+using Problem;
+namespace ProblemDrawer;
+
+
+public class CarrierAssignmentDrawer : ProblemDrawer<ProblemOutputStep>
+{
+    public CarrierAssignmentDrawer(List<ProblemOutputStep> steps) : base(steps)
+    {
+    }
+    protected override Task Draw()
+    {
+        throw new NotImplementedException();
+    }
+}
