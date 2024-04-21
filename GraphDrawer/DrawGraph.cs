22a66b71a77e4af73fc7be99179d79746d1b9f57
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-04-21 22:29:35 +0200

    Small changes to DrawGraph

--- GraphDrawer/DrawGraph.cs
+++ GraphDrawer/DrawGraph.cs
@@ -5,30 +5,30 @@ namespace GraphDrawer;
 public class DrawGraph
 {
     private readonly Canvas2DContext _context;
-    public List<GraphVertex> Vertices;
-    public List<GraphEdge> Edges;
+    private readonly List<GraphVertex> _vertices;
+    private readonly List<GraphEdge> _edges;
 
-    public DrawGraph(Canvas2DContext _context, ref List<GraphVertex> vertexes, ref List<GraphEdge> edges)
+    public DrawGraph(Canvas2DContext context, ref List<GraphVertex> vertexes, ref List<GraphEdge> edges)
     {
-        this._context = _context;
-        this.Vertices = vertexes;
-        this.Edges = edges;
+        _context = context;
+        _vertices = vertexes;
+        _edges = edges;
     }
 
-    public DrawGraph(Canvas2DContext _context, ref GraphInput input)
+    public DrawGraph(Canvas2DContext context, ref GraphInput input)
     {
-        this._context = _context;
-        this.Vertices = input.Vertices;
-        this.Edges = input.Edges;
+        _context = context;
+        _vertices = input.Vertices;
+        _edges = input.Edges;
     }
 
     public async Task ChangeVerticeStatus(int index, GraphState state) {
-        Vertices[index].State = state;
-        await DrawVertex(Vertices[index]);
+        _vertices[index].State = state;
+        await DrawVertex(_vertices[index]);
     }
 
     public async Task ChangeEdgeStatus(int index, GraphState state) {
-        var currentEdge = Edges[index];
+        var currentEdge = _edges[index];
         currentEdge.State = state;
         await DrawEdge(currentEdge);
 
@@ -40,16 +40,16 @@ public class DrawGraph
     {
         await ClearCanvas();
 
-        foreach (var edge in Edges) {
+        foreach (var edge in _edges) {
             await DrawEdge(edge);
         }
 
-        foreach(var vertex in Vertices) {
+        foreach(var vertex in _vertices) {
             await DrawVertex(vertex);
         }
     }
 
-    public async Task DrawEdge(GraphEdge e)
+    private async Task DrawEdge(GraphEdge e)
     {
         await _context.SetStrokeStyleAsync(e.State.GetPrimaryColor());
         await _context.SetLineWidthAsync(e.State.GetLineWidth());
@@ -61,7 +61,7 @@ public class DrawGraph
         await _context.StrokeAsync();
     }
 
-    public async Task DrawVertex(GraphVertex v) {
+    private async Task DrawVertex(GraphVertex v) {
             // Drawing circle outline
             await _context.SetFillStyleAsync(v.State.GetPrimaryColor());
             await _context.BeginPathAsync();
@@ -78,8 +78,8 @@ public class DrawGraph
             await _context.SetFillStyleAsync(v.State.GetPrimaryColor());
             await _context.SetFontAsync("26px Cascadia Mono");
             var text = v.Value.ToString();
-            var text_width = await _context.MeasureTextAsync(text);
-            var x = v.X - text_width.Width / 2;
+            var textWidth = await _context.MeasureTextAsync(text);
+            var x = v.X - textWidth.Width / 2;
             var y = v.Y + 7;
 
             await _context.FillTextAsync(text, x, y);
