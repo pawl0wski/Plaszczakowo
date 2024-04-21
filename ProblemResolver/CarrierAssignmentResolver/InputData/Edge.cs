271202b8f32ae2081895851a2b3c8bae6c3f2fff
6ajmon <6ajmon@gmail.com>
2024-04-21 22:15:18 +0200

    Test with razor

--- ProblemResolver/CarrierAssignmentResolver/InputData/Edge.cs
+++ ProblemResolver/CarrierAssignmentResolver/InputData/Edge.cs
@@ -7,11 +7,12 @@ public class Edge
     public int Flow;
     public int Capacity;
 
-    public Edge(int from, int to, int flow, int capacity) {
+    public Edge(int from, int to, int flow = 0, int capacity = 1) {
         this.From = from;
         this.To = to;
         this.Flow = flow;
         this.Capacity = capacity;
     }
+    
 
 }
