8c43bd71a9f6dec8b2b97ca6d9951cbbfaa1dca5
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-04-22 00:49:23 +0200

    Add GraphCanvas

--- JsDependencies/index.js
+++ JsDependencies/index.js
@@ -1,2 +1,21 @@
 import "animate.css"
 import "@fortawesome/fontawesome-free/css/all.css"
+import panzoom from "panzoom";
+
+window.getWindowWidth = function () {
+    return window.innerWidth;
+}
+
+window.getWindowHeight = function () {
+    return window.innerHeight;
+}
+
+window.enableCanvasZoom = function () {
+    let canvas = document.querySelector("canvas");
+    
+    panzoom(canvas, {
+        zoomSpeed: 0.065,
+        maxZoom: 2,
+        minZoom: 0.5,
+    });
+}
