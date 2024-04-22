6cdb74839b9dba836acbbb2bde11643a28ad78e7
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2024-04-22 14:57:40 +0200

    Change Width and Height to CanvasWidth and CanvasHeight

--- JsDependencies/index.js
+++ JsDependencies/index.js
@@ -2,14 +2,6 @@ import "animate.css"
 import "@fortawesome/fontawesome-free/css/all.css"
 import panzoom from "panzoom";
 
-window.getWindowWidth = function () {
-    return window.innerWidth;
-}
-
-window.getWindowHeight = function () {
-    return window.innerHeight;
-}
-
 window.enableCanvasZoom = function () {
     let canvas = document.querySelector("canvas");
     
