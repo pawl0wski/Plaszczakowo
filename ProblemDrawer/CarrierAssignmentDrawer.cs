bbc7400d8272dce3951c656e0b233bb7e252f7db
6ajmon <6ajmon@gmail.com>
2024-04-23 22:40:59 +0200

    Implement method for creating graph from input data

--- ProblemDrawer/CarrierAssignmentDrawer.cs
+++ ProblemDrawer/CarrierAssignmentDrawer.cs
@@ -6,6 +6,8 @@ using GraphDrawer;
 
 public class CarrierAssignmentDrawer : ProblemDrawer<CarrierAssignmentInputData, CarrierAssignmentOutputStep>
 {
+    public List<GraphVertex> graphVertex;
+    public List<GraphEdge> graphEdge;
     public CarrierAssignmentDrawer(List<CarrierAssignmentOutputStep> steps, GraphDrawer graphDrawer) : base(steps, graphDrawer)
     {
     }
@@ -15,6 +17,16 @@ public class CarrierAssignmentDrawer : ProblemDrawer<CarrierAssignmentInputData,
     }
     public override void CreateGraphDataFromInputData(CarrierAssignmentInputData data)
     {
-        throw new NotImplementedException();
+        
+        for (int i = 0; i < data.FrontCarrierNumber; i++){
+            graphVertex.Add(new GraphVertex(200, i * 100));
+        }
+        for (int i = data.FrontCarrierNumber; i < data.RearCarrierNumber; i++){
+            graphVertex.Add(new GraphVertex(500, i * 100));
+        }
+        for (int i = 0; i < data.Relations.Count; i++)
+        {
+            graphEdge.Add(new GraphEdge(graphVertex[data.Relations[i].From], graphVertex[data.Relations[i].To], null, new GraphFlow(data.Relations[i].Flow, data.Relations[i].Capacity)));
+        }
     }
 }
