a5de19e5f55e1f16c799429086e8478aaa307e58
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2024-05-01 00:18:26 +0200

    Some changes to InputMethods

--- Components/Pages/Input/InputChoiceBase.cs
+++ Components/Pages/Input/InputChoiceBase.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace ProjektZaliczeniowy_AiSD2.Components.Pages.Input;
 
-public class InputChoicePageBase : ComponentBase
+public class InputChoiceBase : ComponentBase
 {
     [Parameter] 
     public required string ProblemName { get; set; }
