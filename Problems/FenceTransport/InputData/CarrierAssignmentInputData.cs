5853c883b516bce594eb2ffbfb31b5ad769fe2db
6ajmon <6ajmon@gmail.com>
2024-05-09 13:56:15 +0200

    add graph input

--- Problems/FenceTransport/InputData/CarrierAssignmentInputData.cs
+++ Problems/FenceTransport/InputData/CarrierAssignmentInputData.cs
@@ -1,10 +1,16 @@
 using ProblemResolver;
+using ProblemResolver.Graph;
 
 namespace Problem.CarrierAssignment;
 
-public record CarrierAssignmentInputData(int FrontCarrierNumber, int RearCarrierNumber, List<Edge> Relations) : ProblemInputData
+public record CarrierAssignmentInputData(int FrontCarrierNumber, int RearCarrierNumber, List<Edge> Relations, 
+    List<ProblemEdge> Roads, List<ProblemVertex> Nodes, int FactoryIndex) : ProblemInputData
 {
     public int FrontCarrierNumber { get; set; } = FrontCarrierNumber;
     public int RearCarrierNumber { get; set; } = RearCarrierNumber;
     public List<Edge> Relations { get; set; } = Relations;
+    
+    public List<ProblemEdge> Roads { get; set; } = Roads;
+    public List<ProblemVertex> Nodes { get; set; } = Nodes;
+    public int FactoryIndex { get; set; } = FactoryIndex;
 }
