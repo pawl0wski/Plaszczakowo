03686a5060f2e365e3438ff8ccf0668328bad170
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-05-13 22:12:46 +0200

    Add Directed Graph

--- Drawer/GraphDrawer/GraphEdge.cs
+++ Drawer/GraphDrawer/GraphEdge.cs
@@ -6,16 +6,19 @@ public class GraphEdge : ICloneable
 
     public readonly GraphVertex To;
 
+    public readonly bool Directed;
+    
     public GraphThroughput? Throughput;
 
     public GraphState State;
 
-    public GraphEdge(GraphVertex from, GraphVertex to, GraphState? state = null, GraphThroughput? throughput = null)
+    public GraphEdge(GraphVertex from, GraphVertex to, GraphState? state = null, GraphThroughput? throughput = null, bool directed = false)
     {
         From = from;
         To = to;
         State = state ?? GraphStates.Inactive;
         Throughput = throughput;
+        Directed = directed;
     }
 
     public object Clone()
