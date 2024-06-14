161fd30cdbe34cea613ca7408f060a0a7caa1f9a
Jakub Pawlowski <59766830+pawl0wski@users.noreply.github.com>
2024-06-14 22:36:27 +0200

    Show ConvexHull in ConvexHullOutput

--- Problems/FenceTransport/ProblemOutput/ConvexHullOutput.cs
+++ Problems/FenceTransport/ProblemOutput/ConvexHullOutput.cs
@@ -1,3 +1,4 @@
+using Drawer.GraphDrawer;
 using ProblemResolver;
 
 namespace Problem.FenceTransport;
@@ -5,5 +6,27 @@ namespace Problem.FenceTransport;
 public record ConvexHullOutput : ProblemOutput
 {
     public List<int>? HullIndexes { get; set; } = new();
+
+    public GraphData ToGraphData(FenceTransportInputData inputData, bool directed = true)
+    {
+        GraphData graphData = new();
+
+        if (HullIndexes == null)
+            throw new NullReferenceException(nameof(HullIndexes));
+        
+        foreach (var vertexId in HullIndexes)
+        {
+            graphData.Vertices.Add(inputData.Vertices.First(v => v.Id == inputData.Vertices[vertexId].Id).ToGraphVertex());
+        }
+
+        for (var i = 1; i < graphData.Vertices.Count; i++)
+        {
+            graphData.AddEdge(new GraphEdge(graphData.Vertices[i-1], graphData.Vertices[i], directed: directed));
+        }
+        graphData.AddEdge(new GraphEdge(graphData.Vertices.Last(), graphData.Vertices.First(), directed: directed));
+
+        return graphData;
+    }
+    
 }
 
