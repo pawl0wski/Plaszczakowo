ab453436aac164563d0c27fa9fd7cafec19c4d45
Bartłomiej Wiśniewski <148913398+bwisniewski26@users.noreply.github.com>
2024-06-14 14:41:12 +0200

    Introduce data verifying in Keyboard Input

---
+++ Components/Pages/Input/InputMethods/KeyboardInput/Validator/KeyboardInputError.cs
@@ -0,0 +1,6 @@
+﻿namespace ProjektZaliczeniowy_AiSD2.Components.Pages.Input.InputMethods.KeyboardInput.Validator;
+
+public record KeyboardInputError(string Content)
+{
+    public readonly string Content = Content;
+}
---
+++ Components/Pages/Input/InputMethods/KeyboardInput/Validator/KeyboardInputValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjektZaliczeniowy_AiSD2.Components.Pages.Input.InputMethods.KeyboardInput.Validator;
+
+public class KeyboardInputValidator
+{
+    public static List<KeyboardInputError> Validate(string Input)
+    {
+        List<KeyboardInputError> errors = new();
+        List<char> distinctChars = new();
+        if (Input.Length < 2)
+            errors.Add(new("Fraza jest zbyt krótka!"));
+        foreach (char c in Input)
+        {
+            if (!distinctChars.Contains(c))
+                distinctChars.Add(c);
+        }
+
+        if (distinctChars.Count < 2)
+            errors.Add(new("Niewystarczająca ilość różnych liter!"));
+
+        return errors;
+    }
+}
